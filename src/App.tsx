36026b4f8b77b85e02e0e8ea218d6fa2aa0688ac
Alberto Bollettin <albertobollettin37@gmail.com>
2023-01-10 12:13:41 +0100

    feat: add header and drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Routes from "./Routes"
+import Routes from './Routes'
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Routes />
-    </div>
+    </>
   )
 }
 
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,38 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
+import DashboardLayout from './layouts/dashboard'
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <div>Hello world!</div>,
+    path: '/dashboard',
+    element: <DashboardLayout />,
+    children: [
+      { element: <Navigate to="/dashboard/books" />, index: true },
+      { path: 'books', element: <div>Books page</div> },
+      { path: 'authors', element: <div>Authors page</div> },
+      { path: 'users', element: <div>Users page</div> },
+    ],
   },
-]);
+  {
+    path: 'login',
+    element: <div>Login page</div>,
+  },
+  {
+    element: <div>Default page</div>,
+    children: [
+      // this redirect doesn't work
+      { element: <Navigate to="/dashboard/books" />, index: true },
+      { path: '404', element: <>404 page</> },
+      { path: '/', element: <Navigate to="/dashboard/app" />, index: true },
+      { path: '*', element: <Navigate to="/404" /> },
+    ],
+  },
+  {
+    path: '*',
+    element: <Navigate to="/404" replace />,
+  },
+])
 
-const Routes: React.FC<{}> = () => {
+const Routes: React.FC = () => {
   return (
     <RouterProvider router={router} />
   )
